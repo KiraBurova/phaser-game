525300fcf7abd08166e1c1f66219c280dae8ce1a
Burova Kira <burovakira11@gmail.com>
2019-08-22 09:36:51 +0300

    add config

---
+++ game/config.js
@@ -0,0 +1,16 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scene: {
+        create: create
+    }
+};
+
+export default config;
